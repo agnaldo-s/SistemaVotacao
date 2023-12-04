2356fadd4ae58cfbfdc19c10606133dbabd1a6ae
agnaldo <gui.agnaldo@gmail.com>
2023-12-04 20:17:25 -0300

    criacão models

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Enquete(models.Model):
+    pergunta = models.CharField(max_length=255)
+    opcao1 = models.CharField(max_length=255)
+    opcao2 = models.CharField(max_length=255)
+    opcao3 = models.CharField(max_length=255)
+    opcao4 = models.CharField(max_length=255)
+    opcao5 = models.CharField(max_length=255)
+    opcao6 = models.CharField(max_length=255)
+    criador = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    
+class Voto(models.Model):
+    enquete = models.ForeignKey(Enquete, blank=False, on_delete=models.CASCADE)
+    resposta = models.CharField(max_length=10)
+    votante = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    
