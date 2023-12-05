8fa8f4cee9563964c6f7dfcde7e548df9287c431
Anderson Dutra <andersonrdutra7@gmail.com>
2023-12-05 16:40:37 -0300

    Editado accounts

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Enquete, Voto
 
-# Register your models here.
+class VotoInline(admin.TabularInline):
+    model = Voto
+    extra = 1
+
+class EnqueteAdmin(admin.ModelAdmin):
+    list_display = ["id", "pergunta", "get_criador_da_enquete"]
+    search_fields = ["pergunta", "criador_da_enquete__username"]
+    inlines = [VotoInline]
+
+    def get_criador_da_enquete(self, obj):
+        return obj.criador_da_enquete.username
+
+    get_criador_da_enquete.short_description = 'Criador da Enquete'
+
+class VotoAdmin(admin.ModelAdmin):
+    list_display = ["id", "enquete", "resposta", "get_usuario_votante"]
+    search_fields = ["enquete__pergunta", "usuario_votante__username"]
+
+    def get_usuario_votante(self, obj):
+        return obj.usuario_votante.username
+
+    get_usuario_votante.short_description = 'Usuário Votante'
+
+admin.site.register(Enquete, EnqueteAdmin)
+admin.site.register(Voto, VotoAdmin)
--- forms/urls.py
+++ forms/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('home_enquetes/', views.index, name='enquetes'),
 ]
