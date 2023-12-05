4c4aa3a7603e12eee003534fe2bac8b724a94e55
LUCAS PEREIRA COELHO <lucascoelho441@gmail.com>
2023-12-05 20:32:01 -0300

    index

--- forms/views.py
+++ forms/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, "pages/index.html")
+
+def add_enquete(request):
+    return render(request, "pages/adicionar_enquete.html")
